eca7e639f336149c75f236feaae695574e204b21
DPLT <dplt1992@gmail.com>
2018-03-09 20:09:26 +0000

    inicio da programação da calculadora

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -9,8 +9,37 @@ namespace CalculadoraCompleta.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet] //esta anatoção e facultativa
         public ActionResult Index()
         {
+            ViewBag.visor = 0;
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt,string visor)
+        {
+            //identificar o valor da variavel 'bt'
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt;
+
+
+                    break;
+            }
+            ViewBag.visor = visor;
+
             return View();
         }
     }
